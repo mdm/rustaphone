3dfabb2620bd7df9db480c178c055c3ec2ad3f7b
Marc Dominik Migge <marcmigge@gmx.net>
2025-02-24 19:47:18 +0100

    Implement API for Rustaphone type

--- src/internal.rs
+++ src/internal.rs
@@ -1,17 +1,20 @@
-use std::{cell::RefCell, rc::Rc};
+use std::sync::{Arc, Mutex};
 
 const HI_OCTAVE: u8 = 8;
 const MAX_TRACKS: usize = 64;
 const MAX_CHANNELS: usize = 64;
 
 const UNUSED_VOICE: Option<Voice> = None;
-const UNUSED_CHANNEL: Option<Rc<RefCell<Rustaphone>>> = None;
+const UNUSED_CHANNEL: Option<Rustaphone> = None;
 
+#[derive(Clone, Default, PartialEq)]
 pub(super) enum State {
+    #[default]
     Stop,
     Play,
 }
 
+#[derive(Clone)]
 pub(super) enum Waveform {
     Square,
     Sawtooth,
@@ -42,6 +45,7 @@ pub(super) enum FxCommand {
     Repeat,
 }
 
+#[derive(Clone)]
 pub(super) struct Params {
     r#type: Waveform,
 
@@ -144,8 +148,18 @@ pub(super) struct Track {
     params: Params,
 }
 
+#[derive(Clone)]
+struct Phaser([f32; 1024]);
+
+impl Default for Phaser {
+    fn default() -> Self {
+        Self([0.0; 1024])
+    }
+}
+
+#[derive(Clone, Default)]
 pub(super) struct Voice {
-    track: Option<Rc<RefCell<Track>>>,
+    track: Option<Arc<Mutex<Track>>>,
     params: Params,
     frames: i32,
     nextnote: [i32; 2],
@@ -166,7 +180,7 @@ pub(super) struct Voice {
     phasex: i32,
     fphase: f32,
     dphase: f32,
-    phaser: [f32; 1024],
+    phaser: Phaser,
     noise: [f32; 32],
     filter: [f32; 8],
     vibe: f32,
@@ -180,79 +194,80 @@ pub(super) struct Voice {
 }
 
 impl Voice {
-    fn reset() {}
-}
+    fn reset(&mut self) {
+        self.period = 100.0 / (self.params.freq as f64 * self.params.freq as f64 + 0.001);
+        self.maxperiod = 100.0 / (self.params.limit as f64 * self.params.limit as f64 + 0.001);
+        self.slide = 1.0 - f64::powf(self.params.slide as f64, 3.0) * 0.01;
+        self.dslide = -f64::powf(self.params.dslide as f64, 3.0) * 0.000001;
+        self.square = 0.5 - self.params.square * 0.5;
+        self.sweep = -self.params.sweep * 0.00005;
+        self.arp = if self.params.arp >= 0.0 {
+            1.0 - f64::powf(self.params.arp as f64, 2.0) * 0.9
+        } else {
+            1.0 + f64::powf(self.params.arp as f64, 2.0) * 10.0
+        };
+        self.atime = 0;
+        self.alimit = if self.params.aspeed == 1.0 {
+            0
+        } else {
+            (f32::powf(1.0 - self.params.aspeed, 2.0) * 20000.0 + 32.0) as i32
+        };
+    }
 
-impl Default for Voice {
-    fn default() -> Self {
-        let params = Params::default();
-        let filter2 = f32::powf(params.lpf, 3.0) * 0.1;
-        let filter4 = 5.0 / (1.0 + f32::powf(params.resonance, 2.0) * 20.0) * (0.01 + filter2);
-        let fphase = f32::powf(params.phase, 2.0) * 1020.0;
-        let dphase = f32::powf(params.psweep, 2.0) * 1.0;
-        let limit = (f32::powf(1.0 - params.repeat, 2.0) * 20000.0 + 32.0) as i32;
+    fn start(&mut self) {
+        self.phase = 0;
+        let filter2 = f32::powf(self.params.lpf, 3.0) * 0.1;
+        let filter4 = 5.0 / (1.0 + f32::powf(self.params.resonance, 2.0) * 20.0) * (0.01 + filter2);
+        self.filter = [
+            0.0,
+            0.0,
+            filter2,
+            1.0 + self.params.lsweep * 0.0001,
+            f32::min(filter4, 0.8),
+            0.0,
+            f32::powf(self.params.hpf, 2.0) * 0.1,
+            1.0 + self.params.hsweep * 0.0003,
+        ];
 
-        Self {
-            track: None,
-            params: Params::default(),
-            frames: 0,
-            nextnote: [0, 0],
-            volume: 0.0,
-            state: State::Play,
-            stage: 0,
-            time: 0,
-            length: [
-                (params.attack * params.attack * 100000.0) as i32,
-                (params.sustain * params.sustain * 100000.0) as i32,
-                (params.decay * params.decay * 100000.0) as i32,
-            ],
-            period: 100.0 / (params.freq as f64 * params.freq as f64 + 0.001),
-            maxperiod: 100.0 / (params.limit as f64 * params.limit as f64 + 0.001),
-            slide: 1.0 - f64::powf(params.slide as f64, 3.0) * 0.01,
-            dslide: -f64::powf(params.dslide as f64, 3.0) * 0.000001,
-            square: 0.5 - params.square * 0.5,
-            sweep: -params.sweep * 0.00005,
-            phase: 0,
-            iphase: i32::abs(fphase as i32),
-            phasex: 0,
-            fphase: if params.phase >= 0.0 { fphase } else { -fphase },
-            dphase: if params.psweep >= 0.0 {
-                dphase
-            } else {
-                -dphase
-            },
-            phaser: [0.0; 1024],
-            noise: rand::random::<[f32; 32]>().map(|r| 2.0 * r - 1.0), // array of random f32s
-            filter: [
-                0.0,
-                0.0,
-                filter2,
-                1.0 + params.lsweep * 0.0001,
-                f32::min(filter4, 0.8),
-                0.0,
-                f32::powf(params.hpf, 2.0) * 0.1,
-                1.0 + params.hsweep * 0.0003,
-            ],
-            vibe: 0.0,
-            vspeed: f32::powf(params.vspeed, 2.0) * 0.01,
-            vdelay: params.vibe * 0.5,
-            repeat: 0,
-            limit: if params.repeat == 0.0 { 0 } else { limit },
-            arp: if params.arp >= 0.0 {
-                1.0 - f64::powf(params.arp as f64, 2.0) * 0.9
-            } else {
-                1.0 + f64::powf(params.arp as f64, 2.0) * 10.0
-            },
-            atime: 0,
-            alimit: if params.aspeed == 1.0 {
-                0
-            } else {
-                (f32::powf(1.0 - params.aspeed, 2.0) * 20000.0 + 32.0) as i32
-            },
-        }
+        self.vibe = 0.0;
+        self.vspeed = f32::powf(self.params.vspeed, 2.0) * 0.01;
+        self.vdelay = self.params.vibe * 0.5;
+
+        self.volume = 0.0;
+        self.stage = 0;
+        self.time = 0;
+        self.length = [
+            (self.params.attack * self.params.attack * 100000.0) as i32,
+            (self.params.sustain * self.params.sustain * 100000.0) as i32,
+            (self.params.decay * self.params.decay * 100000.0) as i32,
+        ];
+
+        let fphase = f32::powf(self.params.phase, 2.0) * 1020.0;
+        self.fphase = if self.params.phase >= 0.0 {
+            fphase
+        } else {
+            -fphase
+        };
+        let dphase = f32::powf(self.params.psweep, 2.0) * 1.0;
+        self.dphase = if self.params.psweep >= 0.0 {
+            dphase
+        } else {
+            -dphase
+        };
+        self.iphase = i32::abs(self.fphase as i32);
+        self.phasex = 0;
+
+        self.phaser = Default::default();
+        self.noise = rand::random::<[f32; 32]>().map(|r| 2.0 * r - 1.0); // array of random f32s
+
+        self.repeat = 0;
+        let limit = (f32::powf(1.0 - self.params.repeat, 2.0) * 20000.0 + 32.0) as i32;
+        self.limit = if self.params.repeat == 0.0 { 0 } else { limit };
+        self.state = State::Play;
     }
 }
 
+#[derive(Clone)]
 pub(super) struct Rustaphone {
     tempo: i32,
     volume: f32,
@@ -261,8 +276,8 @@ pub(super) struct Rustaphone {
 }
 
 impl Rustaphone {
-    pub fn new() -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Rustaphone {
+    pub fn new() -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(Rustaphone {
             tempo: 120,
             volume: 0.10,
             voices: [UNUSED_VOICE; MAX_TRACKS],
@@ -276,9 +291,51 @@ impl Rustaphone {
         }
     }
 
-    fn set_track_at(&mut self, track: Option<Rc<RefCell<Track>>>, index: usize) {
+    pub fn tempo(&mut self, tempo: i32) {
+        self.tempo = tempo;
+    }
+
+    pub fn play(&mut self) {
+        for i in 0..MAX_TRACKS {
+            if let Some(voice) = &mut self.voices[i] {
+                if let Some(track) = &voice.track {
+                    voice.params = track.lock().unwrap().params.clone();
+                    voice.reset();
+                    voice.start();
+                    voice.frames = 0;
+                    voice.nextnote = [0; 2];
+                }
+            }
+        }
+
+        self.state = State::Play;
+    }
+
+    pub fn stop(&mut self) {
+        self.state = State::Stop;
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.state == State::Stop
+    }
+
+    fn set_track_at(&mut self, track: Option<Arc<Mutex<Track>>>, index: usize) {
         self.voices[index] = match self.voices[index].take() {
-            Some(old_voice) => Some(Voice { track, ..old_voice }),
+            Some(old_voice) => {
+                let params = match &track {
+                    Some(track) => track.lock().unwrap().params.clone(),
+                    None => old_voice.params,
+                };
+
+                Some(Voice {
+                    track,
+                    params,
+                    state: State::Stop,
+                    frames: 0,
+                    nextnote: [0; 2],
+                    ..old_voice
+                })
+            }
             None => Some(Voice {
                 track,
                 ..Default::default()
@@ -287,8 +344,12 @@ impl Rustaphone {
     }
 }
 
+pub struct StopHandle {
+    channel: usize,
+}
+
 pub(super) struct Mix {
-    channels: [Option<Rc<RefCell<Rustaphone>>>; MAX_CHANNELS],
+    channels: [Option<Rustaphone>; MAX_CHANNELS],
 }
 
 impl Mix {
@@ -298,13 +359,36 @@ impl Mix {
         }
     }
 
-    pub fn remove(&mut self, rustaphone: Rc<RefCell<Rustaphone>>) {
+    pub fn play(&mut self, mut rustaphone: Rustaphone) -> Option<StopHandle> {
+        rustaphone.play();
+
         for i in 0..MAX_CHANNELS {
-            if let Some(channel) = &self.channels[i] {
-                if Rc::ptr_eq(channel, &rustaphone) {
-                    self.channels[i] = None;
+            match &self.channels[i] {
+                Some(old) if old.state != State::Stop => continue,
+                _ => {
+                    self.channels[i] = Some(rustaphone);
+                    return Some(StopHandle { channel: i });
                 }
             }
         }
+
+        None
+    }
+
+    pub fn stop(&mut self, handle: StopHandle) -> bool {
+        if let Some(channel) = &mut self.channels[handle.channel] {
+            channel.stop();
+        }
+        self.is_done()
+    }
+
+    fn is_done(&self) -> bool {
+        self.channels.iter().all(|channel| {
+            if let Some(channel) = channel {
+                channel.is_done()
+            } else {
+                true
+            }
+        })
     }
 }
